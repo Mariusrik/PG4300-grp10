ef008c012b748bec39e7c71610446ad8f9b2c73d
rikmar15 <rikmar15@student.westerdals.no>
2017-05-08 02:30:53 +0200

    Added bootstrap, basic header, and books as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'books#index', as: 'home'
   resources :comments
   resources :ratings
   resources :users
   resources :books
   resources :categories
-  root to: 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
