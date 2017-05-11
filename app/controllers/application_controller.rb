2bf0b9f8d0c55d9296ceb40fffe867561616c3b7
odeand14 <aodegaar@gmail.com>
2017-05-11 17:52:52 +0200

    Created user privileges-row migration, started on log out cache clearing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :set_cache_buster
   helper_method :current_user
+  helper_method :require_login
 
 
   private
@@ -13,6 +15,12 @@ class ApplicationController < ActionController::Base
     unless current_user
       redirect_to home_url
     end
+  end
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510210652) do
+ActiveRecord::Schema.define(version: 20170511122734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170510210652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_salt"
+    t.string "user_profile", default: "user"
   end
 
   add_foreign_key "books", "categories"
