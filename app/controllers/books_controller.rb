9c7b06dc722e8d9ff9086d2523df335035d59914
odeand14 <aodegaar@gmail.com>
2017-05-13 03:03:13 +0200

    Fixed rating so users can only rate once for each book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,19 @@ class BooksController < ApplicationController
 
   attr_accessor :books
   helper_method :get_avg
+  helper_method :re_rate
 
   def get_avg(num)
     @get_avg = Rating.where(:book_id => num).average(:score)
+  end
+
+  def re_rate(book)
+    @rating_search = Rating.where(:book_id => book).where(:user_id => current_user.id)
+    if @rating_search.present?
+      edit_rating_path(@rating_search.pluck(:id))
+    else
+      new_rating_path(:book => book)
+    end
   end
 
   # GET /books
