ce3de1965cc0f245a3ae626edcde45b2d240bfc1
Thomas Gabrielsen <thomas.gabrielsen@outlook.com>
2017-05-15 18:06:35 +0200

    Updated fixtures and tests for user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -21,27 +21,39 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create user" do
-    #assert_difference('User.count') do
-    #  post users_url, params: { user: { email: 'bob@bob.no', name: 'bob' , password_digest: 'password', password_salt: 'salt', user_profile: 'user'} }
-    #end
+    assert_difference('User.count') do
+      post users_url, params: { user: {
+          email: 'bob@bobbob.no',
+          name: 'bob' ,
+          password_digest: 'secret123',
+          password_digest_confirmation: 'secret123',
+          user_profile: 'user',
+          }}
+    end
 
-    #assert_redirected_to user_url(User.last)
+    #assert_redirected_to user_url(User.last) TODO: Set correct redirect
   end
 
   test "should show user" do
+    controller.session[:user_id] = user.id
     get user_url(@user)
     assert_response :found
   end
 
-  test "should get edit" do
-    #get edit_user_url(@user)
-    #assert_response :success
+  test "should not_get edit_without_correct_rights" do
+    get edit_user_url(@user)
+    assert_response :found
   end
 
-  test "should update user" do
-    #patch user_url(@user), params: { user: { email: @user.email, name: @user.name, password: @user.password, userId: @user.userId } }
+  #test "should update user" do
+  #  patch user_url(@user), params: { user: {
+  #      email: @user.email,
+  #      name: @user.name,
+  #      password: @user.password,
+  #      userId: @user.userId } }
+
     #assert_redirected_to user_url(@user)
-  end
+  #end
 
   test "should destroy user" do
     #assert_difference('User.count', -1) do
