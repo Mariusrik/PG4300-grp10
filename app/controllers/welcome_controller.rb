41aa4ccb2492cf44f532427bfd1b619fb22cdd02
odeand14 <aodegaar@gmail.com>
2017-05-17 18:13:37 +0200

    Added auto-increase/decrease on commentcounter for books plus clearing of commentfield

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,26 +1,29 @@
 class WelcomeController < ApplicationController
-  before_action :index
   skip_before_action :require_login, :only => [:index]
 
 helper_method :get_avg
 helper_method :re_rate
+helper_method :give_rating
 
 def index
   @books = Book.all
+  @ratings = Rating.all
   @rating = Rating.new
 end
+
 
 def get_avg(num)
   @get_avg = Rating.where(:book_id => num).average(:score)
 end
 
+  #need to fix these
+
 def re_rate(book)
-  @rating_search = Rating.where(:book_id => book).where(:user_id => current_user.id)
-  if @rating_search.present?
-    true
-  else
-    false
-  end
+  @has_rating = Rating.where(:book_id => book).where(:user_id => current_user.id).any?
+end
+
+def give_rating(book)
+  @give_rating = Rating.where(:book_id => book).where(:user_id => current_user.id).pluck(:id)
 end
 
 
